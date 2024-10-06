8551fcaa581857cd64520e2c920ce4c9d46e1fb7
yuyue1999 <yuyue1260@foxmail.com>
2024-10-06 16:18:00 -0400

    lint

--- test_main.py
+++ test_main.py
@@ -1,44 +1,28 @@
-"""
-This module contains unit tests for the database operations in main.py.
-"""
-
 import pytest
 import main
 
 @pytest.fixture
-def db_connection():
-    """
-    Fixture to provide an in-memory database connection for testing.
-    """
+def setup_db():
     conn = main.open_database(":memory:")
     main.create_users_table(conn)
     yield conn
     conn.close()
 
-def test_add_user(db_connection):
-    """
-    Test adding a user to the database.
-    """
-    main.add_user(db_connection, "Joey", 24)
-    users = main.fetch_all_users(db_connection)
+def test_add_user(setup_db):
+    main.add_user(setup_db, "Joey", 24)
+    users = main.fetch_all_users(setup_db)
     assert len(users) == 1
     assert users[0][1] == "Joey"
     assert users[0][2] == 24
 
-def test_modify_user_age(db_connection):
-    """
-    Test modifying a user's age.
-    """
-    main.add_user(db_connection, "Joey", 24)
-    main.modify_user_age(db_connection, 1, 18)
-    users = main.fetch_all_users(db_connection)
+def test_modify_user_age(setup_db):
+    main.add_user(setup_db, "Joey", 24)
+    main.modify_user_age(setup_db, 1, 18)
+    users = main.fetch_all_users(setup_db)
     assert users[0][2] == 18
 
-def test_remove_user(db_connection):
-    """
-    Test removing a user from the database.
-    """
-    main.add_user(db_connection, "Joey", 24)
-    main.remove_user(db_connection, 1)
-    users = main.fetch_all_users(db_connection)
+def test_remove_user(setup_db):
+    main.add_user(setup_db, "Joey", 24)
+    main.remove_user(setup_db, 1)
+    users = main.fetch_all_users(setup_db)
     assert len(users) == 0
